fb1b2fe480a8bf6663b354967e3ae45df9587338
Huends-Jhoebert <huendahuenda20@gmail.com>
2021-04-05 04:14:37 +0800

    added hover effects at lis

--- js/todo.js
+++ js/todo.js
@@ -1,17 +1,34 @@
-
+// get the li parent
 var listItems = document.querySelector('.list-items');
 
+// clas task
 class Task {
 
+	// constructor
 	constructor(_task) {
 		this.task = _task;
 	}
 
+	// adding method
 	add() {
+		// crete li
 		let newLi = document.createElement('li');
 		let textNode = document.createTextNode(this.task);
 		newLi.appendChild(textNode);
 		newLi.className = 'li';
+
+		// create a link
+		let newA = document.createElement('a');
+		newA.setAttribute('href', '#');
+		newLi.appendChild(newA);
+
+		//create a delete icon
+		let newIcon = document.createElement('i');
+		newIcon.id = 'remove';
+		newIcon.className = 'fas fa-trash';
+		newA.appendChild(newIcon);
+
+		// append the li child to the ul parent
 		listItems.appendChild(newLi);
 	}
 }
@@ -20,7 +37,6 @@ class Task {
 var userInput = document.querySelector('#user-input');
 var submitBtn = document.querySelector('#submit-btn');
 submitBtn.addEventListener('click', addUserToClassObject);
-var userNumber = 0;
 function addUserToClassObject() {
 	event.preventDefault();
 	let user1 = new Task(userInput.value);
