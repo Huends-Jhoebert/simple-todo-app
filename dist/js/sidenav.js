7363b7530277c95df192e2b3a17b965540d2aea1
Huends-Jhoebert <huendahuenda20@gmail.com>
2021-08-07 00:59:38 +0800

    initialized prefered sidenav animation and its scss structure

---
+++ dist/js/sidenav.js
@@ -0,0 +1,13 @@
+function openNav() {
+   document.querySelector(".new-task-btn").style.display = "none";
+   document.getElementById("mySidenav").style.width = "100%";
+   //document.getElementById("main").style.marginLeft = "250px";
+   //    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+   document.querySelector(".new-task-btn").style.display = "inline-block";
+   document.getElementById("mySidenav").style.width = "0";
+   //    document.getElementById("main").style.marginLeft = "0";
+   //    document.body.style.backgroundColor = "white";
+}
