04f7d9603366b3b2de582e14c91a0e92e9d66b1e
Huends-Jhoebert <huendahuenda20@gmail.com>
2021-08-12 01:12:10 +0800

    added animation that when a checkbox is clicked or selected, the delete button will show

---
+++ dist/js/listAnimation.js
@@ -0,0 +1,13 @@
+function showHide(idName) {
+   // ! if checkbox is check show the delete button
+   if (document.querySelector(`#${idName}`).checked) {
+      var getBtn = (document.querySelector(
+         `#${idName}`
+      ).parentNode.parentNode.childNodes[2].style.display = "inline");
+   } else if (document.querySelector(`#${idName}`).checked == false) {
+      // ! else hide the button
+      var getBtn = (document.querySelector(
+         `#${idName}`
+      ).parentNode.parentNode.childNodes[2].style.display = "none");
+   }
+}
